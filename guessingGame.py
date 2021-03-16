c5870612c7c321b2cbbb43781fade159b03dd4cc
aisha109 <62790685+aisha109@users.noreply.github.com>
2021-03-16 12:08:51 +0300

    Add files via upload

---
+++ guessingGame.py
@@ -0,0 +1,11 @@
+Question = "Guess a number between 1 and 9"
+print(Question)
+QuestionInput = input("Enter your guess")
+
+
+
+if(QuestionInput == 8 ):
+    print("YOU ARE RIGHT")
+else:
+    print("ANOTHER GUESS")
+
